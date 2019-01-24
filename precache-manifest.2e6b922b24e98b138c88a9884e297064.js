5b17324a3323599038e1b5631fda048eee9f9f91
rebyoliveira <rebecca.d.oliveira@gmail.com>
2019-01-24 13:33:51 +0000

    Updates

---
+++ precache-manifest.2e6b922b24e98b138c88a9884e297064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62eee420ff6360f6f428",
+    "url": "/estudar_comvc/static/js/runtime~main.62eee420.js"
+  },
+  {
+    "revision": "c4b10fea4dbccd09873a",
+    "url": "/estudar_comvc/static/js/main.c4b10fea.chunk.js"
+  },
+  {
+    "revision": "fcfd3c649117c807899a",
+    "url": "/estudar_comvc/static/js/1.fcfd3c64.chunk.js"
+  },
+  {
+    "revision": "c4b10fea4dbccd09873a",
+    "url": "/estudar_comvc/static/css/main.e6665ad5.chunk.css"
+  },
+  {
+    "revision": "fcfd3c649117c807899a",
+    "url": "/estudar_comvc/static/css/1.304a7810.chunk.css"
+  },
+  {
+    "revision": "90f4d9ed917affc3aa306a7e3b52c967",
+    "url": "/estudar_comvc/index.html"
+  }
+];
